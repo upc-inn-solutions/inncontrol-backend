d25b7da6fe9a7d8176d627d0a278ccdb81955d50
Fernando Salgado <fs142610@gmail.com>
2026-05-10 17:05:53 -0500

    fix: add TaskScheduler for heartbeats

--- src/main/java/com/inncontrol/config/WebSocketConfig.java
+++ src/main/java/com/inncontrol/config/WebSocketConfig.java
@@ -1,7 +1,10 @@
 package com.inncontrol.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -13,9 +16,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic", "/queue")
-              .setHeartbeatValue(new long[]{10000, 10000}); // Heartbeat cada 10 segundos
+              .setHeartbeatValue(new long[]{10000, 10000})
+              .setTaskScheduler(heartBeatScheduler());
         config.setApplicationDestinationPrefixes("/app");
         config.setUserDestinationPrefix("/user");
+    }
+
+    @Bean
+    public TaskScheduler heartBeatScheduler() {
+        return new ThreadPoolTaskScheduler();
     }
 
     @Override
